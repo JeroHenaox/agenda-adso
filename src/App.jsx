2e7a23032b2f3d8717009a07aefc2eba8c86ac51
JeroHenaox <jeroxyt62@gmail.com>
2026-07-22 16:50:54 -0500

    primer trabajo con react

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  const fecha = new Date().toLocaleString();
+  return(
+    <main>
+      <h1>Hola SENA</h1>
+      <h2>Fecha y hora actual: {fecha}</h2>
+      <p>Soy Jeronimo Henao y esta es mi primeraclase de ReactJS</p>
+    </main>
+  );
+}
