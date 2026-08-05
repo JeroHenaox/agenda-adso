8c4384a76b91724164d355a2b6352207eeeda156
JeroHenaox <jeroxyt62@gmail.com>
2026-08-05 15:07:51 -0500

    Agregar dos contactos adicionales a la agenda

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,66 @@
+import "./App.css";
+import ContactoCard from "./components/ContactoCard";
+
 export default function App() {
-  const fecha = new Date().toLocaleString();
+  // Esta es nuestra "base de datos" inicial quemada en el código
+ const contactos = [
+  {
+    id: 1,
+    nombre: "Carolina Pérez",
+    telefono: "300 123 4567",
+    correo: "carolina@sena.edu.co",
+    etiqueta: "Compañera",
+  },
+  {
+    id: 2,
+    nombre: "Juan Díaz",
+    telefono: "301 987 6543",
+    correo: "juan@sena.edu.co",
+    etiqueta: "Instructor",
+  },
+  {
+    id: 3,
+    nombre: "Luisa Martínez",
+    telefono: "320 555 7788",
+    correo: "luisa@sena.edu.co",
+    etiqueta: "Cliente",
+  },
+  {
+    id: 4,
+    nombre: "Andrés Gómez",
+    telefono: "315 456 7890",
+    correo: "andres@sena.edu.co",
+    etiqueta: "Aprendiz",
+  },
+  {
+    id: 5,
+    nombre: "María Rodríguez",
+    telefono: "318 654 3210",
+    correo: "maria@sena.edu.co",
+    etiqueta: "Compañera",
+  },
+];
+
   return (
-    <main>
-      <h1>Hola SENA</h1>
-      <h2>Fecha y hora actual: {fecha}</h2>
-      <p>Soy Jeronimo Henao y esta es mi primeraclase de ReactJS</p>
+    <main className="app-container">
+      <h1 className="app-title">Agenda ADSO 📒</h1>
+
+      <p className="app-subtitle">Contactos guardados</p>
+
+      {/* Recorremos el arreglo contactos y pintamos una tarjeta por cada uno */}
+      {contactos.map((c) => (
+        <ContactoCard
+          key={c.id}            // key única para React
+          nombre={c.nombre}     // prop nombre
+          telefono={c.telefono} // prop telefono
+          correo={c.correo}     // prop correo
+          etiqueta={c.etiqueta} // prop etiqueta (Cliente, Instructor, etc.)
+        />
+      ))}
+
+      <p className="app-nota">
+        (Versión 0.1 - solo lectura, sin agregar ni editar todavía)
+      </p>
     </main>
   );
 }
